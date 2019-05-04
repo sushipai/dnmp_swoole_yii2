1a28f7df61e80758dd106d76f872e8ac8d6507f2
sushipai <42344344@qq.com>
2019-05-04 21:09:59 +0800

    init

---
+++ www/easyswoole/App/HttpController/Router.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2018/8/15
+ * Time: 上午10:39
+ */
+
+namespace App\HttpController;
+
+
+use EasySwoole\Http\AbstractInterface\AbstractRouter;
+use FastRoute\RouteCollector;
+
+class Router extends AbstractRouter
+{
+
+    function initialize(RouteCollector $routeCollector)
+    {
+        $routeCollector->get('/','WebSocket/index');
+    }
+}
